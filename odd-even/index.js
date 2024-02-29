5dd09d494e0cda642300f5dc27ec779c70eea676
RokasKontrimas <rokas.kontrimas123@gmail.com>
2024-02-29 11:21:09 +0200

    first task

---
+++ odd-even/index.js
@@ -0,0 +1,14 @@
+// Task 1
+let num1 = 5;
+let num2 = 10;
+console.groupCollapsed('Task1')
+if (num1 > num2) {
+    console.log(`${num2} is greater than ${num1}`)
+} else if (num1 === num2) {
+    console.log(`${num2} and ${num1} are equal`)
+} else {
+    console.log(`${num1} is greater than ${num2}`)
+}
+console.groupEnd()
+
+
